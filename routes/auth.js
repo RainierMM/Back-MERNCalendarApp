59975fa48532a5066cc3c46982edb7658a0d8fde
Rainier <rainiermichelena@gmail.com>
2021-03-28 11:49:00 -0500

    Backend 1.0 terminado

---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+/*
+        RUTAS DE USUARIOS / Auth
+        Host + /api/auth
+*/
+
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require("../controllers/auth");
+
+router.post(
+  "/new",
+  [
+    // middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos,
+  ],
+  crearUsuario
+);
+
+router.post(
+  "/",
+  [
+    // middlewares
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos,
+  ],
+  loginUsuario
+);
+
+router.get("/renew", validarJWT, revalidarToken);
+
+module.exports = router;
